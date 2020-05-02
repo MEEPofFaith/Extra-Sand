50277b490357a583a78eabbad77cfc8cc009be51
MEEP of Faith <meepmatthew@gmail.com>
2020-05-02 01:01:08 -0700

    slower rainbow

--- scripts/overseer.js
+++ scripts/overseer.js
@@ -1,8 +1,8 @@
 const engineTrail = newEffect(39, e => {
 	Draw.blend(Blending.normal);
-  Draw.color(Color.valueOf("#b30000").shiftHue(Time.time() * 8));
+  Draw.color(Color.valueOf("#b30000").shiftHue(Time.time() * 1.5));
 	Fill.circle(e.x, e.y, e.fout() * 2.5);
-	Draw.color(Color.valueOf("#ff0000").shiftHue(Time.time() * 8));
+	Draw.color(Color.valueOf("#ff0000").shiftHue(Time.time() * 1.5));
 	Fill.circle(e.x, e.y + 0.25 , e.fout() * 1.5);
 	Draw.blend();
 });
