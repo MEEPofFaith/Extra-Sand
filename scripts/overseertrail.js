36330a1b1e9c4bb903ed489b5ea37b9741b65508
MEEP of Faith <meepmatthew@gmail.com>
2020-05-01 18:30:23 -0700

    split?

--- scripts/overseertrail.js
+++ scripts/overseertrail.js
@@ -1,22 +1,24 @@
-const trail = newEffect(39, e => {
+const trailBot = newEffect(39, e => {
 	Draw.blend(Blending.additive);
   Draw.color(Color.valueOf("#f3b380"), Color.valueOf("#f7c7a3"), e.fin());
 	Fill.circle(e.x, e.y, ((1 * e.fout()) * e.rotation) / 0.8);
+	Draw.blend();
+});
+const trailTop = newEffect(39, e => {
+	Draw.blend(Blending.additive);
 	Draw.color(Color.valueOf("#edcdb4"), Color.valueOf("#e8d8cc"), e.fin());
 	Fill.circle(e.x, e.y, ((1 * e.fout()) * e.rotation) / 1.7);
 	Draw.blend();
-	
-	//Draw.color(Color.valueOf("ffffff"));
-	//Fill.circle(e.x, e.y, (1 * e.fout()) * (e.rotation / 1.3));
 });
 
 const chaos = extendContent(Mech, "overseer, {
-  updateAlt(player){
+  updateAlt: function(player){
     this.super$updateAlt(player);
     
     const veccc = new Vec2();
     
     veccc.trns(this.rotation, 0, 7.5);
-    Effects.effect(trail, this.x + veccc.x, this.y + veccc.y, this.rotation);
+    Effects.effect(trailBot, this.x + veccc.x, this.y + veccc.y, this.rotation);
+    Effects.effect(trailTop, this.x + veccc.x, this.y + veccc.y, this.rotation);
   }
 });
