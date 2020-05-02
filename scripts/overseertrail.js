6f9523cfd8e054ce95340ff009f6e3cb42a62b15
MEEP of Faith <meepmatthew@gmail.com>
2020-05-01 20:44:45 -0700

    Update overseertrail.js

--- scripts/overseertrail.js
+++ scripts/overseertrail.js
@@ -1,11 +1,7 @@
-const trailBot = newEffect(39, e => {
+const engineTrail = newEffect(39, e => {
 	Draw.blend(Blending.additive);
   Draw.color(Color.valueOf("#f3b380"), Color.valueOf("#f7c7a3"), e.fin());
 	Fill.circle(e.x, e.y, ((1 * e.fout()) * e.rotation) / 0.8);
-	Draw.blend();
-});
-const trailTop = newEffect(39, e => {
-	Draw.blend(Blending.additive);
 	Draw.color(Color.valueOf("#edcdb4"), Color.valueOf("#e8d8cc"), e.fin());
 	Fill.circle(e.x, e.y, ((1 * e.fout()) * e.rotation) / 1.7);
 	Draw.blend();
@@ -15,9 +11,8 @@ const chaos = extendContent(Mech, "overseer", {
   updateAlt(player){
     const veccc = new Vec2();
     
-    veccc.trns(this.rotation, 0, 7.5);
-    Effects.effect(trailBot, this.x + veccc.x, this.y + veccc.y, this.rotation);
-    Effects.effect(trailTop, this.x + veccc.x, this.y + veccc.y, this.rotation);
+    veccc.trns(player.rotation, 0, 7.5);
+    Effects.effect(engineTrail, this.x + veccc.x, this.y + veccc.y, this.rotation);
   }
 });
 
