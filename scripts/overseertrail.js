bb1459a5998884427afe72bf3e9f5a5cb8ce92d0
MEEP of Faith <meepmatthew@gmail.com>
2020-05-01 17:32:47 -0700

    trail

---
+++ scripts/overseertrail.js
@@ -0,0 +1,22 @@
+const trail = newEffect(39, e => {
+	Draw.blend(Blending.additive);
+  Draw.color(Color.valueOf("#f3b380"), Color.valueOf("#f7c7a3"), e.fin());
+	Fill.circle(e.x, e.y, ((1 * e.fout()) * e.rotation) / 0.8);
+	Draw.color(Color.valueOf("#edcdb4"), Color.valueOf("#e8d8cc"), e.fin());
+	Fill.circle(e.x, e.y, ((1 * e.fout()) * e.rotation) / 1.7);
+	Draw.blend();
+	
+	//Draw.color(Color.valueOf("ffffff"));
+	//Fill.circle(e.x, e.y, (1 * e.fout()) * (e.rotation / 1.3));
+});
+
+const chaos = extendContent(Mech, "overseer, {
+  update(){
+    this.super$update();
+    
+    const veccc = new Vec2();
+    
+    veccc.trns(this.rotation, 0, this.engineOffset);
+    Effects.effect(trail, this.x + veccc.x, this.y + veccc.y, this.rotation);
+  }
+});
